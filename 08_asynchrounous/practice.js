6464ff3a1ee751c0610b35b23d98dd083b0f5b07
juyun <juyun571@naver.com>
2024-11-20 11:25:31 +0900

    비동기 처리 practice

--- 08_asynchrounous/practice.js
+++ 08_asynchrounous/practice.js
@@ -1,38 +1,88 @@
-function login(name) {
+// 기존 콜백 코드
+function call(name, cb) {
+  setTimeout(function () {
+    console.log(name);
+    cb(name);
+  }, 1000);
+}
+
+function back(cb) {
+  setTimeout(function () {
+    console.log("back");
+    cb("back");
+  }, 1000);
+}
+
+function hell(cb) {
+  setTimeout(function () {
+    cb("callback hell");
+  }, 1000);
+}
+
+// 콜백 함수 실행
+call("kim", function (name) {
+  console.log(name + " 반가워");
+  back(function (txt) {
+    console.log(txt + "를 실행했구나");
+    hell(function (msg) {
+      console.log("여기는 " + msg);
+    });
+  });
+});
+
+//promise
+function callPromise(name) {
   return new Promise(function (resolve, reject) {
     setTimeout(function () {
-      resolve(name);
+      console.log(name);
+      resolve(name); // cb 대신 resolve로 값 넘기기
     }, 1000);
   });
 }
 
-function getVideo(txt) {
-  return new Promise(function (resolve, reject) {
+function backPromise() {
+  return new Promise((resolve, reject) => {
     setTimeout(function () {
-      resolve(txt + "을 실행했구나");
+      console.log("back");
+      resolve("back");
     }, 1000);
   });
 }
 
-function getDetail(message) {
-  return new Promise(function (resolve, reject) {
+function hellPromise() {
+  // reject 생략 가능
+  return new Promise((resolve) => {
     setTimeout(function () {
-      resolve("여기는" + message);
+      resolve("callback hell");
     }, 1000);
   });
 }
 
-// call("kim")
-//   .then(() => back("back"))
-//   .then(() => hell("callback hell"));
+//promise
+// then ~ catch
+callPromise("kim")
+  .then((result) => {
+    console.log(result + " 반가워");
+    return backPromise();
+  })
+  .then((txt) => {
+    // txt = 이전 then의 리턴값 = "back"
+    console.log(txt + "을 실행했구나");
+    return hellPromise();
+    // return "안녕하세요";
+  })
+  .then((msg) => {
+    console.log("여기는 " + msg);
+  });
 
-async function exec() {
-  let user = await login("kim");
-  console.log(user + "님 환영합니다.");
-  let videos = await getVideo("back");
-  console.log(videos);
-  let title = await getDetail("callback hell");
-  console.log(title);
+//   async/await
+async function execute() {
+  const name = await callPromise("allie");
+  console.log(name + "반가워");
+  const back = await backPromise();
+  console.log(back + "을 실행했구나");
+  const hell = await hellPromise();
+  console.log("여기는 " + hell);
 }
 
-exec();
+execute();
